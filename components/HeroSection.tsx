93c1748214019c8dc18fe8bc92b75293a9b8169a
mahnoorjanjua505 <mano.flora11@gmail.com>
2025-02-04 17:52:38 +0500

    update

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,7 +7,7 @@ import { HiArrowDown } from "react-icons/hi"
 const HeroSection = () => {
   return (
     <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-4  sm:mt-52 py-15 sm:py-32 md:py-40 md:flex-row md:space-x-4 md:text-left"> 
+            <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className=" sm:mt-52 md:mt-2 md:w-1/2">
           <Image
             src="/me.jpeg"
